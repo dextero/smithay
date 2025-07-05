708d13f8efa2629c5b88cdb3fe3ce7ff5f53026c
Marcin Radomski <marcin@mradomski.pl>
2025-07-05 21:03:07 +0200

    log to stderr

--- smallvil/src/main.rs
+++ smallvil/src/main.rs
@@ -21,9 +21,11 @@ pub struct CalloopData {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     if let Ok(env_filter) = tracing_subscriber::EnvFilter::try_from_default_env() {
-        tracing_subscriber::fmt().with_env_filter(env_filter).init();
+        tracing_subscriber::fmt().with_writer(std::io::stderr).with_env_filter(env_filter).init();
     } else {
-        tracing_subscriber::fmt().init();
+        tracing_subscriber::fmt()
+        .with_writer(std::io::stderr
+        ).init();
     }
 
     let mut event_loop: EventLoop<CalloopData> = EventLoop::try_new()?;
